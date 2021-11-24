dfc118e05193247db39dbfcd4dc446721be0ce50
Anartz Mugika Ledo <mugan86@gmail.com>
2021-11-24 20:20:29 +0100

    Post Add Todo

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Todo } from './todo.interface';
 import { TodoService } from './todo.service';
 
@@ -6,8 +6,12 @@ import { TodoService } from './todo.service';
 export class TodoController {
     constructor(private readonly todoService: TodoService) {}
 
+    @Post()
+    create(@Body() todo: Todo): void {
+        this.todoService.create(todo);
+    }
     @Get()
     findAddl(): Todo[] {
-        return ;
+        return this.todoService.findAddl();
     }
 }
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -4,4 +4,14 @@ import { Todo } from './todo.interface';
 @Injectable()
 export class TodoService {
     private storage: Todo[] = [];
+
+    create(todo: Todo): void {
+        const currentMaxId = Math.max(...this.storage.map((t: Todo) => t.id));
+        todo.id = currentMaxId > 0 ? currentMaxId + 1 : 0;
+        this.storage.push(todo);
+    }
+
+    findAddl(): Todo[] {
+        return this.storage;
+    }
 }
