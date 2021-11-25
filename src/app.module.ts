84cc7621749b4a740f017eed309c46fc08e9744e
Anartz Mugika Ledo <mugan86@gmail.com>
2021-11-25 19:16:41 +0100

    Nest API complete

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Todo } from './todo/entities/todo.entity';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
-    TypeOrmModule.forFeature([Todo]),
     TodoModule,
   ],
   controllers: [AppController],
